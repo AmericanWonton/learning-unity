a7b3cef9423d54a1c86cdc3cb03635eabb873c7d
missusdever <missusMGdess@gmail.com>
2023-02-05 16:08:17 -0600

    Finished 3.1

--- create-with-code/Prototype_3/Prototype_3/Assets/Scripts/SpawnManager.cs
+++ create-with-code/Prototype_3/Prototype_3/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,24 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Used to spawn obstacles
+    void SpawnObstacle ()
+    {
+        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
     }
 }
